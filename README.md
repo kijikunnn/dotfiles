e4abae76eca6684078ade44849e09fd459d5e0d7
kijikunnn <kijikunnn@gmail.com>
2024-11-11 19:18:49 +0900

    feat:add configs

--- README.md
+++ README.md
@@ -8,13 +8,6 @@
 
 2.
 
-## How to create Brewfile
-
-```sh
-# Even if exists Brewfile, --force is overwriting
-brew bundle dump --force
-```
-
 ## Configures
 
 - [fish](https://fishshell.com/)
@@ -22,6 +15,22 @@ brew bundle dump --force
   - [fish-bd](https://github.com/0rax/fish-bd)
   - [tide](https://github.com/IlanCosman/tide)
 
-## Hand settings
+## Handle settings
 
 - [GitHub Copilot](https://docs.github.com/ja/copilot/using-github-copilot/getting-code-suggestions-in-your-ide-with-github-copilot?tool=vscode)
+
+- ## iTerm2
+
+## Work on the source Device
+
+### Create Brewfile
+
+```sh
+# Even if exists Brewfile, --force is overwriting
+brew bundle dump --force
+```
+
+### Create iTerm2 settings file
+
+1. open settings window (iTerm2 > Settings)
+2. export user settings (General > Settings > "Export All Settings ...")
---
+++ configs/fish/config.fish
@@ -0,0 +1,53 @@
+### MANAGED BY RANCHER DESKTOP START (DO NOT EDIT)
+set --export --prepend PATH "/Users/kijikunnn/.rd/bin"
+### MANAGED BY RANCHER DESKTOP END (DO NOT EDIT)
+
+### alias
+# reload shell
+alias reload='exec $SHELL -l'
+
+# cd
+alias ..='cd ..'
+
+# ls
+if test (command -v eza)
+  alias e='eza --icons'
+  alias l='e'
+  alias ls='e'
+  alias ea='eza -a --icons'
+  alias la='ea'
+  alias ee='eza -aal --icons'
+  alias ll='ee'
+  alias et='eza -T -L 3 -a -I "node_modules|.git|.cache" --icons'
+  alias lt='et'
+  alias eta='eza -T -a -I "node_modules|.git|.cache" --color=always --icons | less -r'
+  alias lta='eta'
+end
+
+# cat
+alias cat='bat'
+
+# vim
+alias vi='nvim'
+alias vim='nvim'
+
+# open app
+alias rov='open -a "/Applications/RustRover.app"'
+alias ogc='open -a "Google Chrome"'
+### alias end
+
+# Auto open BetterTouchTool
+if not pgrep -x "BetterTouchTool" > /dev/null
+    open -a "/Applications/BetterTouchTool.app"
+end
+
+# pnpm
+set -gx PNPM_HOME "/Users/kijikunnn/Library/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
+
+set -gx WASMTIME_HOME "$HOME/.wasmtime"
+
+string match -r ".wasmtime" "$PATH" > /dev/null; or set -gx PATH "$WASMTIME_HOME/bin" $PATH
