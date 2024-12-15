5e1ad4919fd9ee56768e2a0dcecfa1ee193391ea
kijikunnn <kijikunnn@gmail.com>
2024-12-15 21:08:11 +0900

    feat:config.fishにbrewの環境設定を追加

--- configs/fish/config.fish
+++ configs/fish/config.fish
@@ -51,3 +51,5 @@ end
 set -gx WASMTIME_HOME "$HOME/.wasmtime"
 
 string match -r ".wasmtime" "$PATH" > /dev/null; or set -gx PATH "$WASMTIME_HOME/bin" $PATH
+
+eval "$(/opt/homebrew/bin/brew shellenv)"
